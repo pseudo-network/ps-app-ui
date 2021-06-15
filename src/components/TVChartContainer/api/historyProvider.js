bf20228ba4e26f549f1191af012ebfff7b9f2b67
rrossilli55 <rrossilli55@gmail.com>
2021-06-15 10:15:04 -0400

    progress

--- src/components/TVChartContainer/api/historyProvider.js
+++ src/components/TVChartContainer/api/historyProvider.js
@@ -1,25 +1,14 @@
 var rp = require("request-promise").defaults({ json: true })
 
-const api_root = "https://min-api.cryptocompare.com"
+// todo: should come from .env
+const api_root = "http://localhost:3444"
 const history = {}
 
 export default {
   history: history,
 
   getBars: function (symbolInfo, resolution, from, to, first, limit) {
-    // var split_symbol = symbolInfo.name.split(/[:/]/)
-    const url = `http://localhost:3444/history?from=${from}&to=${to}&resolution=${resolution}`
-    // const qs = {
-    //   e: split_symbol[0],
-    //   fsym: split_symbol[1],
-    //   tsym: split_symbol[2],
-    //   toTs: to ? to : "",
-    //   limit: limit ? limit : 2000,
-    //   // aggregate: 1//resolution
-    // }
-    // console.log({qs})
-
-    console.log(resolution)
+    const url = `${api_root}/history?from=${from}&to=${to}&resolution=${resolution}`
 
     return rp({
       url: `${url}`,
@@ -30,21 +19,10 @@ export default {
         console.log("CryptoCompare API error:", data.Message)
         return []
       }
-      // console.log("=.=.==.=.==.=.=.==.=.=.==")
-      // console.log(data)
-      // console.log("=.=.==.=.==.=.=.==.=.=.==")
+
+      // todo: revise to check for errors
       if (data.length > 0) {
-        // console.log(
-        //   `Actually returned: ${new Date(
-        //     data.TimeFrom * 1000
-        //   ).toISOString()} - ${new Date(data.TimeTo * 1000).toISOString()}`
-        // )
-        // console.log("=.==.==.=.=.==.")
-        // console.log(data)
-        // console.log("=.==.==.=.=.==.")
-
         var bars = []
-
         bars = data.map(res => {
           return {
             time: res.unixTimeMS,
@@ -56,16 +34,6 @@ export default {
           }
         })
 
-        // var bars = data.map(el => {
-        //   return {
-        //     time: el.time * 1000, //TradingView requires bar time in ms
-        //     low: el.l,
-        //     high: el.h,
-        //     open: el.o,
-        //     close: el.c,
-        //     volume: el.v,
-        //   }
-        // })
         if (first) {
           var lastBar = bars[bars.length - 1]
           history[symbolInfo.name] = { lastBar: lastBar }
@@ -78,66 +46,3 @@ export default {
     })
   },
 }
-
-// var rp = require("request-promise").defaults({ json: true })
-
-// const api_root = "https://min-api.cryptocompare.com"
-// const history = {}
-
-// export default {
-//   history: history,
-
-//   getBars: function (symbolInfo, resolution, from, to, first, limit) {
-//     var split_symbol = symbolInfo.name.split(/[:/]/)
-//     const url =
-//       resolution === "D"
-//         ? "/data/histoday"
-//         : resolution >= 60
-//         ? "/data/histohour"
-//         : "/data/histominute"
-//     const qs = {
-//       e: split_symbol[0],
-//       fsym: split_symbol[1],
-//       tsym: split_symbol[2],
-//       toTs: to ? to : "",
-//       limit: limit ? limit : 2000,
-//       // aggregate: 1//resolution
-//     }
-//     console.log({ qs })
-
-//     return rp({
-//       url: `${api_root}${url}`,
-//       qs,
-//     }).then(data => {
-//       console.log({ data })
-//       if (data.Response && data.Response === "Error") {
-//         console.log("CryptoCompare API error:", data.Message)
-//         return []
-//       }
-//       if (data.Data.length) {
-//         console.log(
-//           `Actually returned: ${new Date(
-//             data.TimeFrom * 1000
-//           ).toISOString()} - ${new Date(data.TimeTo * 1000).toISOString()}`
-//         )
-//         var bars = data.Data.map(el => {
-//           return {
-//             time: el.time * 1000, //TradingView requires bar time in ms
-//             low: el.low,
-//             high: el.high,
-//             open: el.open,
-//             close: el.close,
-//             volume: el.volumefrom,
-//           }
-//         })
-//         if (first) {
-//           var lastBar = bars[bars.length - 1]
-//           history[symbolInfo.name] = { lastBar: lastBar }
-//         }
-//         return bars
-//       } else {
-//         return []
-//       }
-//     })
-//   },
-// }
