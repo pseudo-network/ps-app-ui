2ca6ecb553f8630afdd33d0ee7cc9c81febcae8a
rrossilli55 <rrossilli55@gmail.com>
2021-06-22 00:07:10 -0400

    removed outliers from chart

--- src/components/TVChartContainer/api/historyProvider.js
+++ src/components/TVChartContainer/api/historyProvider.js
@@ -1,33 +1,65 @@
+
 var rp = require("request-promise").defaults({ json: true })
+const math = require('mathjs');
 
 // todo: should come from .env
 const api_root = "http://localhost:3444"
 const history = {}
+const outlier_threshold = 3
+
+function calcZ(x, mean, std){
+  return (x - mean) / std
+}
+
+function findOutliersInArray(arr){
+  const mean = math.mean(arr)
+  const std = math.std(arr)
+
+  var outlierIndexes = []
+  for (let i = 0; i < arr.length; i++) {
+    const a = arr[i];
+    const z = math.abs(calcZ(a, mean, std))
+    if (z > outlier_threshold){
+      outlierIndexes.push(i)
+    }
+  }
+
+  return outlierIndexes
+}
 
 export default {
   history: history,
 
   getBars: function (symbolInfo, resolution, from, to, first, limit) {
-    console.log("=-=-=-=-=-")
-    console.log(symbolInfo)
     const url = `${api_root}/history?from=${from}&to=${to}&resolution=${resolution}`
 
     return rp({
       url: `${url}`,
       // qs,`
     }).then(data => {
-      console.log({ data })
+      console.log("transactions returned: ", data.length)
+
       if (data.Response && data.Response === "Error") {
         console.log("CryptoCompare API error:", data.Message)
         return []
       }
 
       // todo: revise to check for errors
-      if (data.length > 0) {
-        var bars = []
-        bars = data.map(res => {
-          // todo: remove outliers
+      if (data && data.length > 0) {
 
+        let lows = data.map(d => d.low)  
+        let highs = data.map(d => d.high)
+        let opens = data.map(d => d.open)
+        let closes = data.map(d => d.close)
+
+        let outlierIndexes = findOutliersInArray(lows).concat(findOutliersInArray(highs), findOutliersInArray(opens), findOutliersInArray(closes))
+  
+        // nullify outliers in data
+        outlierIndexes.forEach(i=>{
+          data[i] = {}
+        })  
+
+        let bars = data.map(res => {
           return {
             time: res.unixTimeMS,
             low: res.low,
