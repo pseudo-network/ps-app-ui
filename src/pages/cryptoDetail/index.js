564383c722214f54a319d724aa7a28a121b78e54
rrossilli55 <rrossilli55@gmail.com>
2021-06-23 00:00:32 -0400

    progress

--- src/pages/cryptoDetail/index.js
+++ src/pages/cryptoDetail/index.js
@@ -10,37 +10,23 @@ import {
   Grid,
   LinearProgress as MuiLinearProgress,
   Typography,
-} from "@material-ui/core";
-
-import styled, { withTheme } from "styled-components/macro";
-import { spacing } from "@material-ui/system";
+} from "@material-ui/core"
 
 // Redux Components
-import PropTypes from "prop-types"
 import { connect } from "react-redux"
-
 import NavFrame from "../../components/organisms/navigation/MainNavigationFrame" // The top navigation bar and side navigation panel
 
-const drawerWidth = 240;
-
-
-const Divider = styled(MuiDivider)(spacing);
-
-
-
-const CryptoDetail = (props) => {
-    const classes = useStyles();
-  
-    return (
-      <NavFrame page={"CryptoDetail"}>
+const CryptoDetail = props => {
+  return (
+    <NavFrame page={"CryptoDetail"}>
       <Grid container spacing={2} xs={12} sm={12} md={12} lg={12}>
         <Grid item>
-          <TVChartContainer xs={12} sm={12} md={12} lg={12}/>
+          <TVChartContainer xs={12} sm={12} md={12} lg={12} />
         </Grid>
       </Grid>
-      </NavFrame>
-    );
-  }
+    </NavFrame>
+  )
+}
 // Component Properties
 CryptoDetail.propTypes = {}
 
