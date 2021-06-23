79b2496df25879eee0b19d8651b290f6985587ba
rrossilli55 <rrossilli55@gmail.com>
2021-06-22 23:52:57 -0400

    progress

--- src/components/organisms/navigation/MainNavigationFrame.js
+++ src/components/organisms/navigation/MainNavigationFrame.js
@@ -1,271 +1,203 @@
 //React Components and Hooks
-import React, { useState, useRef, useEffect, withStyles } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Redirect,
-  useHistory,
-  useLocation,
-  withRouter
-} from "react-router-dom";
+import React, { useState, useRef, useEffect, withStyles } from "react"
+import { withRouter } from "react-router-dom"
 
 // Redux Components
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import PropTypes from "prop-types"
+import { connect } from "react-redux"
 
 //Material UI Components
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import ExpandLess from '@material-ui/icons/ExpandLess';
-import ExpandMore from '@material-ui/icons/ExpandMore';
-import Collapse from '@material-ui/core/Collapse';
-import Drawer from '@material-ui/core/Drawer';
-import clsx from 'clsx';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import List from '@material-ui/core/List';
-import IconButton from '@material-ui/core/IconButton';
-import Divider from '@material-ui/core/Divider';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
+import { makeStyles, useTheme } from "@material-ui/core/styles"
+import ListItem from "@material-ui/core/ListItem"
+import ListItemText from "@material-ui/core/ListItemText"
+import ExpandLess from "@material-ui/icons/ExpandLess"
+import ExpandMore from "@material-ui/icons/ExpandMore"
+import Collapse from "@material-ui/core/Collapse"
+import Drawer from "@material-ui/core/Drawer"
+import clsx from "clsx"
+import CssBaseline from "@material-ui/core/CssBaseline"
+import ListItemIcon from "@material-ui/core/ListItemIcon"
+import List from "@material-ui/core/List"
+import IconButton from "@material-ui/core/IconButton"
+import Divider from "@material-ui/core/Divider"
+import Typography from "@material-ui/core/Typography"
+import Box from "@material-ui/core/Box"
+import HomeOutlinedIcon from "@material-ui/icons/HomeOutlined"
+import AppBar from "@material-ui/core/AppBar"
+import Toolbar from "@material-ui/core/Toolbar"
 
-//Material UI Icons
-import ComputerIcon from '@material-ui/icons/Computer';
-import ContactMailIcon from '@material-ui/icons/ContactMail';
-import HomeOutlinedIcon from '@material-ui/icons/HomeOutlined';
-import PowerOffOutlinedIcon from '@material-ui/icons/PowerOffOutlined';
-import ReceiptIcon from '@material-ui/icons/Receipt';
-import MonetizationOnOutlinedIcon from '@material-ui/icons/MonetizationOnOutlined';
-import BuildOutlinedIcon from '@material-ui/icons/BuildOutlined';
-import PeopleOutlineIcon from '@material-ui/icons/PeopleOutline';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import LocalAtmIcon from '@material-ui/icons/LocalAtm';
-import SettingsIcon from '@material-ui/icons/Settings';
-import HomeWork from '@material-ui/icons/HomeWork';
-import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
-import FolderOpenIcon from '@material-ui/icons/FolderOpen';
-import LocalShippingOutlinedIcon from '@material-ui/icons/LocalShippingOutlined';
-import PermMediaOutlinedIcon from '@material-ui/icons/PermMediaOutlined';
-import AccountBalanceIcon from '@material-ui/icons/AccountBalance';
+const drawerWidth = 240
 
-
-
-const drawerWidth = 240;
-
-function ListItemObject (Title, IconName, Location, isDisabled=false) {
+function ListItemObject(Title, IconName, Location, isDisabled = false) {
   return {
-    "title": Title,
-    "icon": IconName,
-    "path": Location,
-    "isDisabled": isDisabled
-  };
+    title: Title,
+    icon: IconName,
+    path: Location,
+    isDisabled: isDisabled,
+  }
 }
 
-function ParentListItemObject (Title, IconName, clickMethod, isOpen, Children = [], isDisabled=false) {
-  return {
-    "title": Title,
-    "icon": IconName,
-    "clickMethod": clickMethod,
-    "isOpen": isOpen,
-    "children": Children,
-    "isDisabled": isDisabled
-  };
-}
-
-function ChildListItemObject (Title, Location, isDisabled=false) {
-  return {
-    "title": Title,
-    "path": Location,
-    "isDisabled": isDisabled
-  };
-}
-
-// Nav list styles:
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   root: {
-    width: '100%',
-    maxWidth: 360,
-    backgroundColor: theme.palette.background.paper,
-  },
-  hide: {
-    display: 'none',
-  },
-  toolbar: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    ...theme.mixins.toolbar,
-  },
-  nested: {
-    paddingLeft: theme.spacing(4),
-  },drawer: {
-    [theme.breakpoints.up('sm')]: {
-      width: drawerWidth,
-      flexShrink: 0,
-    },
+    display: "flex",
   },
   appBar: {
-    [theme.breakpoints.up('sm')]: {
-      width: `calc(100% - ${drawerWidth}px)`,
-      marginLeft: drawerWidth,
-    },
+    zIndex: theme.zIndex.drawer + 1,
   },
-  menuButton: {
-    marginRight: theme.spacing(2),
-    [theme.breakpoints.up('sm')]: {
-      display: 'none',
-    },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
   },
-  // necessary for content to be below app bar
-  toolbar: theme.mixins.toolbar,
   drawerPaper: {
     width: drawerWidth,
   },
+  drawerContainer: {
+    overflow: "auto",
+  },
   content: {
     flexGrow: 1,
-    paddingTop: 50
+    padding: theme.spacing(3),
   },
-}));
+}))
 
+const NavigationPanel = props => {
+  // Where all the possible navigation locations and thier icons and titles are stored
+  const navItems = [
+    ListItemObject("Dashboard", <HomeOutlinedIcon />, "/dashboard"),
+    ListItemObject("Safemoon Tracker", <HomeOutlinedIcon />, "/"),
+    ListItemObject(
+      "Coin Researcher",
+      <HomeOutlinedIcon />,
+      "/coin-research",
+      true
+    ),
+  ]
 
-const NavigationPanel = (props)=> {
-     // Where all the possible navigation locations and thier icons and titles are stored
-     const navItems = [
-      ListItemObject("Dashboard", <HomeOutlinedIcon/>, "/dashboard"),
-      ListItemObject("Safemoon Tracker", <HomeOutlinedIcon/>, "/"),
-      ListItemObject("Coin Researcher", <HomeOutlinedIcon/>, "/coin-research", true),
-    ]
+  const parentNavItems = []
 
-    const parentNavItems = [];
-
-
-
-
-    // Generating and managing function for nav list items:
-    const NavListItem = (({ navItem, key }) => {
-      // Boolean checking if that list item is the current path
-      var isSelected = (navItem.path == props.location.pathname);
-        return (
-          <>
-            {navItem.isDisabled? // if the nav list item is disabled:
-                <ListItem id={key}>
-                  <ListItemIcon>
-                  {navItem.icon}
-                </ListItemIcon>
-                <ListItemText primary={navItem.title} secondary={"Coming Soon!"} />
-              </ListItem>
-              :
-              <ListItem button onClick={(e)=>props.history.push(`${navItem.path}`)} id={key} selected={isSelected}>
-                <ListItemIcon>
-                {navItem.icon}
-              </ListItemIcon>
-              <ListItemText primary={navItem.title} />
-            </ListItem>
-            }
-          </>
-        )
-    });
-
-    const ChildNavListItem = (({ navItem, key }) => {
-      // Boolean checking if that list item is the current path
-      var isSelected = (navItem.path == props.location.pathname);
-        return (
-        <ListItem button onClick={(e)=>props.history.push(`${navItem.path}`)} id={navItem.path + "NavListChildItem" + key} selected={isSelected} className={classes.nested}>
-          <ListItemText primary={navItem.title} />
-        </ListItem>
-        )
-    });
-
-
-    const ParentNavListItems = (({ navItem, key, }) => {
-      // Boolean checking if that list item is the current path
-      var isSelected = (navItem.path == props.location.pathname);
-        return (
-          <>
-          <ListItem button onClick={navItem.clickMethod}>
-            <ListItemIcon>
-              {navItem.icon}
-            </ListItemIcon>
-              <ListItemText primary={navItem.title} />
-              {navItem.isOpen ? <ExpandLess/>: <ExpandMore />}
+  // Generating and managing function for nav list items:
+  const NavListItem = ({ navItem, key }) => {
+    // Boolean checking if that list item is the current path
+    var isSelected = navItem.path == props.location.pathname
+    return (
+      <>
+        {navItem.isDisabled ? ( // if the nav list item is disabled:
+          <ListItem id={key}>
+            <ListItemIcon>{navItem.icon}</ListItemIcon>
+            <ListItemText primary={navItem.title} secondary={"Coming Soon!"} />
           </ListItem>
-          <Collapse in={navItem.isOpen} timeout="auto" unmountOnExit>
-            <List component="div" disablePadding>
+        ) : (
+          <ListItem
+            button
+            onClick={e => props.history.push(`${navItem.path}`)}
+            id={key}
+            selected={isSelected}
+          >
+            <ListItemIcon>{navItem.icon}</ListItemIcon>
+            <ListItemText primary={navItem.title} />
+          </ListItem>
+        )}
+      </>
+    )
+  }
+
+  const ChildNavListItem = ({ navItem, key }) => {
+    // Boolean checking if that list item is the current path
+    var isSelected = navItem.path == props.location.pathname
+    return (
+      <ListItem
+        button
+        onClick={e => props.history.push(`${navItem.path}`)}
+        id={navItem.path + "NavListChildItem" + key}
+        selected={isSelected}
+        className={classes.nested}
+      >
+        <ListItemText primary={navItem.title} />
+      </ListItem>
+    )
+  }
+
+  const ParentNavListItems = ({ navItem, key }) => {
+    // Boolean checking if that list item is the current path
+    var isSelected = navItem.path == props.location.pathname
+    return (
+      <>
+        <ListItem button onClick={navItem.clickMethod}>
+          <ListItemIcon>{navItem.icon}</ListItemIcon>
+          <ListItemText primary={navItem.title} />
+          {navItem.isOpen ? <ExpandLess /> : <ExpandMore />}
+        </ListItem>
+        <Collapse in={navItem.isOpen} timeout="auto" unmountOnExit>
+          <List component="div" disablePadding>
             {navItem.children.map((item, index) => (
-              <ChildNavListItem button navItem={item} key={index}/>
+              <ChildNavListItem button navItem={item} key={index} />
             ))}
-            </List>
-          </Collapse>
-          </>
-        )
-    });
-    const classes = useStyles();
+          </List>
+        </Collapse>
+      </>
+    )
+  }
 
-    return(
-        <div className={classes.root}>
-        <CssBaseline />
-        <AppBar position="fixed" className={classes.appBar}>
-          <Toolbar className={classes.appBar}>
-          </Toolbar>
-        </AppBar>
-        <Drawer
-          className={classes.drawer}
-          variant="permanent"
-          classes={{
-            paper: classes.drawerPaper,
-          }}
-        >
-          <AppBar position="relative">
-            <Toolbar>
-              <Typography variant="h4">
-                Pseudocoin
-              </Typography>
-            </Toolbar>
-          </AppBar>
-          <div className={classes.drawerContainer}>
-            <List>
-            {parentNavItems.map((item, key) => {
-                  //console.log("NavListParentItem" + key);
-                return(
-                <ParentNavListItems button navItem={item} id={item.path + "NavListParentItem" + key}/>
-                );
-              })
-              }
-              {navItems.map((item, key) => {
-                //console.log("NavListItem" + key);
-                return(
-                  <NavListItem navItem={item} id={item.path + "NavListItem" + key}/>
-                  );
-                })
-              }
-            </List>
-            </div>
-        </Drawer>
+  const classes = useStyles()
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6" noWrap>
+            Clipped drawer
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
         <Toolbar />
-        <Divider/>
-            <main className={classes.content}>
-              <div className={classes.appBarSpacer} />
-                <Box m={5} ml={8}>
-                  {props.children}
-                </Box>
-            </main>
+        <div className={classes.drawerContainer}>
+          <List>
+            {parentNavItems.map((item, key) => {
+              //console.log("NavListParentItem" + key);
+              return (
+                <ParentNavListItems
+                  button
+                  navItem={item}
+                  id={item.path + "NavListParentItem" + key}
+                />
+              )
+            })}
+            {navItems.map((item, key) => {
+              //console.log("NavListItem" + key);
+              return (
+                <NavListItem
+                  navItem={item}
+                  id={item.path + "NavListItem" + key}
+                />
+              )
+            })}
+          </List>
         </div>
-      );
+      </Drawer>
+      <Divider />
+      <main className={classes.content}>
+        <div className={classes.appBarSpacer} />
+        <Box m={5} ml={8}>
+          {props.children}
+        </Box>
+      </main>
+    </div>
+  )
 }
-
-
 
 // Component Properties
 NavigationPanel.propTypes = {}
 
 // Component State
 function NavigationPanelState(state) {
-  return {
-  }
+  return {}
 }
-export default connect(NavigationPanelState)(withRouter(NavigationPanel));
+export default connect(NavigationPanelState)(withRouter(NavigationPanel))
--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,63 +1,19 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
-import AppBar from '@material-ui/core/AppBar';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import { TVChartContainer } from '../../components/molecules/TVChartContainer/index';
+import React from "react"
 
 // Redux Components
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import PropTypes from "prop-types"
+import { connect } from "react-redux"
 
-import NavFrame from "../../components/organisms/navigation/MainNavigationFrame"; // The top navigation bar and side navigation panel
+import NavFrame from "../../components/organisms/navigation/MainNavigationFrame" // The top navigation bar and side navigation panel
 
-const drawerWidth = 240;
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-  },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1,
-  },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-  },
-  drawerPaper: {
-    width: drawerWidth,
-  },
-  drawerContainer: {
-    overflow: 'auto',
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-  },
-}));
-
-
-const Dashboard = (props) => {
-    const classes = useStyles();
-  
-    return (
-      <NavFrame page={"CryptoDetail"}>
-            YO!
-      </NavFrame>
-    );
-  }
+const Dashboard = props => {
+  return <NavFrame page={"CryptoDetail"}>YO!</NavFrame>
+}
 // Component Properties
 Dashboard.propTypes = {}
 
 // Component State
 function DashboardState(state) {
-  return {
-  }
+  return {}
 }
-export default connect(DashboardState)(Dashboard);
+export default connect(DashboardState)(Dashboard)
