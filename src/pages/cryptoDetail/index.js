cc28392de19e04119cf8d1c9d468054189ab7967
rrossilli55 <rrossilli55@gmail.com>
2021-06-23 00:03:37 -0400

    progress

--- src/pages/cryptoDetail/index.js
+++ src/pages/cryptoDetail/index.js
@@ -1,37 +1,17 @@
-import {
-  Paper,
-  Avatar as MuiAvatar,
-  Box,
-  Breadcrumbs as MuiBreadcrumbs,
-  Button as MuiButton,
-  Card,
-  CardContent,
-  Divider as MuiDivider,
-  Grid,
-  LinearProgress as MuiLinearProgress,
-  Typography,
-} from "@material-ui/core"
-
-import styled, { withTheme } from "styled-components/macro"
-import { makeStyles, withStyles } from "@material-ui/core/styles"
-import { spacing } from "@material-ui/system"
+import React from "react"
+import { makeStyles } from "@material-ui/core/styles"
+import { TVChartContainer } from "../../components/molecules/TVChartContainer/index"
 
 // Redux Components
+import PropTypes from "prop-types"
 import { connect } from "react-redux"
+
 import NavFrame from "../../components/organisms/navigation/MainNavigationFrame" // The top navigation bar and side navigation panel
-
-const drawerWidth = 240
-
-const Divider = styled(MuiDivider)(spacing)
 
 const CryptoDetail = props => {
   return (
     <NavFrame page={"CryptoDetail"}>
-      <Grid container spacing={2} xs={12} sm={12} md={12} lg={12}>
-        <Grid item>
-          <TVChartContainer xs={12} sm={12} md={12} lg={12} />
-        </Grid>
-      </Grid>
+      <TVChartContainer />
     </NavFrame>
   )
 }
