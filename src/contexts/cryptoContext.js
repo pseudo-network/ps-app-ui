6523e019024520e8e3046333907dd45e8663fdb0
rrossilli55 <rrossilli55@gmail.com>
2021-08-03 19:28:09 -0400

    fixed rounding errors

--- src/contexts/cryptoContext.js
+++ src/contexts/cryptoContext.js
@@ -9,6 +9,27 @@ import {
 } from "../core/environments"
 import axios from "axios"
 
+/**
+ * Checks if number is in exponential format (eg: 1e-8 for 0.00000001).
+ * If it does not, original number is returned.
+ * If it does it converts it to string representation of that number
+ * which forces it to format 0.00000001
+ */
+export function convertExponentialToDecimal(exponentialNumber) {
+  // sanity check - is it exponential number
+  const str = exponentialNumber.toString()
+  if (str.indexOf("e") !== -1) {
+    const exponent = parseInt(str.split("-")[1], 10)
+    // Unfortunately I can not return 1e-8 as 0.00000001, because even if I call parseFloat() on it,
+    // it will still return the exponential representation
+    // So I have to use .toFixed()
+    const result = exponentialNumber.toFixed(exponent + 2)
+    return result
+  } else {
+    return exponentialNumber
+  }
+}
+
 const cryptoContext = createContext()
 
 export function useCrypto() {
@@ -47,7 +68,7 @@ function getCryptoInfoByAddress(address, busd) {
   let quoteCurrency = busd ? BUSD_ADDRESS : WBNB_ADDRESS
   return axios
     .get(
-      `${API_BASE_URL}/cryptos/${address}/info?quote_currency=${quoteCurrency}`
+      `${API_BASE_URL}/cryptos/${address}/info?quote_currency=${WBNB_ADDRESS}`
     )
     .then((res) => {
       if (res.data) {
@@ -118,9 +139,14 @@ function useProvideCrypto() {
 
         if (!res) return
         setBeginningPrice(res.beginning_price)
-        setCurrentPrice(res.current_price)
+        setCurrentPrice(convertExponentialToDecimal(res.current_price))
         setVolume(res.volume)
         setPercentChange(res.percent_change)
+
+        console.log(
+          "currentPrice",
+          convertExponentialToDecimal(res.current_price)
+        )
       })
     }
   }, [address, busd])
